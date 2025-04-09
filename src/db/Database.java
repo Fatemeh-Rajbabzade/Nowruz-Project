d3dcdcc9ec81fe6f6aecf0d0c130fe266a64455c
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-04-09 14:15:22 -0700

    Add registerValidator for adding Validator

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,5 +1,6 @@
 package db;
 import java.util.ArrayList;
+import java.util.HashMap;
 import db.exception.EntityNotFoundException;
 import db.exception.InvalidEntityException;
 
@@ -8,6 +9,13 @@ public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int currentId = 1;
     private static Validator validator;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
+
+    public static void registerValidator(int entityCode, Validator validator) {
+        if (validators.containsKey(entityCode))
+            throw new IllegalArgumentException("Validator with this entityCode exists.");
+    validator.put(entityCode, validator);
+    }
 
     public static void setValidator(Validator validator){
         Database.validator = validator;
@@ -22,12 +30,17 @@ public class Database {
         if (e == null || !validEntity(e)) {
             throw new InvalidEntityException("Your Entity is invalid!");
         }
+
+        Validator validator = validators.get(e.getEntityCode());
         if (validator != null)
             validator.validate(e);
 
+        else
+            throw new IllegalArgumentException("validator whith this entity code " + e.getEntityCode() + " not found.")
         e.id = currentId++;
         entities.add(e.copy());
     }
+
     //پیدا کردن موجودیت با ایدی مدنظر
     public static Entity get(int id) {
         for (Entity e : entities){
@@ -45,8 +58,12 @@ public class Database {
     public static void update(Entity e) throws InvalidEntityException {
         if (e == null || !validEntity(e))
             throw new InvalidEntityException("Your Entity is invalid!");
+        Validator validator = validators.get(e.getEntityCode());
         if (validator != null)
             validator.validate(e);
+
+        else
+            throw new IllegalArgumentException("validator whith this entity code " + e.getEntityCode() + " not found.");
 
         Entity entity = get(e.id);
         entities.remove(entity);
